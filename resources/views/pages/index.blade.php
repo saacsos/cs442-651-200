c3007ab3b5332f09c195e015e29adfd036d7239a
Saac Sornchai <sornchai.la@ku.th>
2022-07-12 16:32:44 +0700

    layout

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,14 @@
+{{-- resources/views/pages/index.blade.php --}}
+@extends('layouts.main')
+
+@section('content')
+    <h1>List of All Pages</h1>
+
+    @for($i = 0; $i <= 10; $i++)
+        <div>
+            <a href="{{ url("/pages/{$i}") }}">
+                Page {{ $i }}
+            </a>
+        </div>
+    @endfor
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return "About Page";
+});
+
+Route::get('/pages', [\App\Http\Controllers\PageController::class, 'index']);
+Route::get('/pages/{id}', [\App\Http\Controllers\PageController::class, 'show']);
