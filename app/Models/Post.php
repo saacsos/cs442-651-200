6ea44b6d7fa23745a704538bf7b69dbd32313f48
Saac Sornchai <sornchai.la@ku.th>
2022-07-19 16:18:04 +0700

    seeder and factory

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,5 +7,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    use HasFactory; // trait 
+    use HasFactory; // trait
+
+    public function scopeAdvertise($query)
+    {
+        return $query->where('like_count', '<', 1000)
+                     ->where('view_count', '>', 70000);
+    }
+
+    public function scopePopular($query, $like_count, $view_count)
+    {
+        return $query->where('like_count', '>=', $like_count)
+                     ->where('view_count', '>=', $view_count);
+    }
+
+    public function scopeFilterTitle($query, $search)
+    {
+        return $query->where('title', 'LIKE', "%{$search}%"); // % wildcard
+    }
 }
